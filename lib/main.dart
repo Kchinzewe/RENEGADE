473eb12c6c6df4f942938a3e67a535b37d549e8f
KChinzewe <kasalwechinzewe@gmail.com>
2022-11-24 17:24:18 +0200

    what: bottom navigation bar why: to allow easier user navigation of various screens

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:connectpamodziv03/terms/privacy_screen.dart';
 import 'package:connectpamodziv03/terms/terms_screen.dart';
 import 'package:connectpamodziv03/signin_screen.dart';
 import 'package:connectpamodziv03/signup_screen.dart';
+import 'package:connectpamodziv03/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -17,6 +18,7 @@ void main() {
       '/cookies': (context) => const CookieScreen(),
       '/signup': (context) => const SignUpScreen(),
       '/signin': (context) => const SignInScreen(),
+      '/home': (context) => const HomeScreen(),
     },
     initialRoute: '/welcome',
   ));
